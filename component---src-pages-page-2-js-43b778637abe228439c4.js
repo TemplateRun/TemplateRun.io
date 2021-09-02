432b4c2a5c6de0fad43c0d79080b26172a4e96e9
sayantan1413 <64970095+sayantan1413@users.noreply.github.com>
2021-09-02 20:30:21 +0000

    Deploying to site from @ TemplateRun/TemplateRun.io@99c06907462e9983c368bd16a9f9caf7a4cce229 🚀

---
+++ component---src-pages-page-2-js-43b778637abe228439c4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{9863:function(e,t,a){a.r(t);var l=a(7294),n=a(5444),r=a(5127),c=a(3751);t.default=function(){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-43b778637abe228439c4.js.map
