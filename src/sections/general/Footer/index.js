c6839f709006b0727c5c41c70c44a0cfed980238
Aditya Chaterjee <speak2adi@gmail.com>
2021-09-04 15:41:49 +0530

    changes
    Signed-off-by: Aditya Chaterjee <speak2adi@gmail.com>

--- src/sections/general/Footer/index.js
+++ src/sections/general/Footer/index.js
@@ -3,6 +3,21 @@ import "./styles.css";
 
 const Footer = () => {
   return (
+    <main class="content" role="main">
+    <h1>Fixed footer, scroll down.</h1>
+    
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A rerum blanditiis soluta nesciunt doloremque voluptate earum tempore fugit eveniet veritatis adipisci magni alias vitae dignissimos incidunt repellat ea quisquam amet eligendi perspiciatis pariatur excepturi magnam omnis aliquam quo aperiam non quibusdam sit molestiae ex. Quasi ipsa libero magnam maiores delectus.</p>
+  
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi vitae perferendis natus porro voluptate molestias deserunt impedit numquam recusandae ducimus dolorem facilis qui autem dolorum quam omnis repellendus asperiores neque excepturi ipsa! Rerum corrupti similique adipisci dolorem cupiditate modi veritatis eum doloribus placeat at quos explicabo et quidem! Illo incidunt obcaecati quo rem nesciunt dolorum fugit vitae voluptates eaque soluta!</p>
+  
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eligendi voluptates suscipit nesciunt nisi illo placeat qui voluptas quas vero voluptatibus eius in voluptate. Itaque eius nobis error id ullam officiis consectetur? Laboriosam commodi excepturi distinctio voluptas. Quam mollitia culpa ipsa? Modi magni officiis quibusdam nulla eum magnam quo in distinctio quis deleniti aliquid minima cum. Assumenda magni quae incidunt aliquam? Omnis eum aspernatur molestiae provident in quod delectus dolor sint vero? Quibusdam tempore similique.</p>
+    
+    <h2>Keep scrolling..</h2>
+    
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet eum aspernatur quibusdam ad provident molestiae adipisci numquam vitae molestias quidem vero nostrum voluptates harum! Vero veniam adipisci minima corporis quidem sunt omnis illum similique consectetur enim atque autem distinctio quas deserunt ex amet itaque ipsa cumque sed asperiores doloremque aliquid praesentium nihil tenetur cum aliquam.</p>
+    
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis ratione blanditiis vero rem cupiditate magni praesentium veniam quibusdam dicta recusandae? Fugit fuga debitis inventore possimus distinctio perferendis.</p>
+  
       <footer class="fixed_footer">
         <div class="content">
           <p>
@@ -15,7 +30,7 @@ const Footer = () => {
           </p>
         </div>
       </footer>
-      
+      </main>
   )
 }
 
